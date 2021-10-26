c7e4857cf350628267e9a79064debeba0b94a5b7
Shiming Zhang <wzshiming@foxmail.com>
2021-10-26 14:03:48 +0800

    First commit

---
+++ checkout.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "$#" -lt 1 ]]; then
+    echo "${0} release: checkout to expected patch release"
+    exit 2
+fi
+
+source "$(dirname "${BASH_SOURCE}")/helper.sh"
+cd "${ROOT}"
+
+if [[ ! -d "${WORKDIR}/.git" ]]; then
+    ${KITDIR}/git_init_workdir.sh
+fi
+
+ORIGIN_RELEASE="$1"
+
+if [[ "${ORIGIN_RELEASE}" =~ ^v1(.[0-9]+){2}$ ]]; then
+    ${KITDIR}/git_fetch_tag.sh "${ORIGIN_RELEASE}"
+    exit
+fi
+
+RELEASE="${ORIGIN_RELEASE%.*}.0"
+
+BASE_RELEASE=$(helper::config::get_base_release ${ORIGIN_RELEASE})
+if [[ "${BASE_RELEASE}" != "" ]]; then
+    RELEASE="${ORIGIN_RELEASE}"
+else
+    BASE_RELEASE=$(helper::config::get_base_release ${RELEASE})
+fi
+
+${KITDIR}/checkout.sh ${BASE_RELEASE}
+
+PATCHES=$(helper::config::get_patches ${RELEASE})
+
+echo "Release ${BASE_RELEASE} patches ($(echo ${PATCHES} | tr ' ' ',')) as release ${RELEASE}"
+
+PATCH_LIST=$(helper::config::get_patch ${PATCHES})
+
+if [[ "${PATCH_LIST}" != "" ]]; then
+    ${KITDIR}/git_patch.sh ${PATCH_LIST}
+fi
+
+cd "${WORKDIR}" && git tag -f "${ORIGIN_RELEASE}"
---
+++ format_all_patch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source "$(dirname "${BASH_SOURCE}")/helper.sh"
+cd "${ROOT}"
+
+PATCHES=$(ls ${PATCHESDIR}/*.patch)
+
+for patch in ${PATCHES}; do
+    ${KITDIR}/format_patch.sh "$patch"
+done
