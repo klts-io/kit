839f588abb1e6783e556eab09784f267d694f209
Shiming Zhang <wzshiming@foxmail.com>
2021-10-28 19:44:40 +0800

    Fix sync repos

--- git_sync_repos.sh
+++ git_sync_repos.sh
@@ -12,25 +12,20 @@ BRANCH_PREFIX="${BRANCH_PREFIX:-}"
 BRANCH="${BRANCH:-${BRANCH_PREFIX}$(helper::workdir::version)}"
 
 if [[ ! -d "${REPOSDIR}/.git" ]]; then
-    mkdir -p "${REPOSDIR}"
-    cd "${REPOSDIR}"
+    mkdir -p "${REPOSDIR}" && cd "${REPOSDIR}" && {
+        git init .
 
-    git init .
+        if [[ "${GH_TOKEN:-}" != "" ]]; then
+            SOURCE=$(echo ${SOURCE} | sed "s#https://github.com#https://bot:${GH_TOKEN}@github.com#g")
+        fi
 
-    if [[ "$(git config user.name)" == "" ]]; then
-        git config --global user.name "bot"
-    fi
-
-    if [[ "${GH_TOKEN:-}" != "" ]]; then
-        SOURCE=$(echo ${SOURCE} | sed "s#https://github.com#https://bot:${GH_TOKEN}@github.com#g")
-    fi
-
-    git remote add "${ORIGIN}" "${SOURCE}"
+        git remote add "${ORIGIN}" "${SOURCE}"
+    }
 fi
 
-cd "${REPOSDIR}"
-
-git add *
-git commit -m "Automatic synchronize ${BRANCH}"
-git branch -M "${BRANCH}"
-git push -u "${ORIGIN}" "${BRANCH}"
+cd "${REPOSDIR}" && {
+    git add *
+    git commit -m "Automatic synchronize ${BRANCH}"
+    git branch -M "${BRANCH}"
+    git push -f -u "${ORIGIN}" "${BRANCH}"
+}
